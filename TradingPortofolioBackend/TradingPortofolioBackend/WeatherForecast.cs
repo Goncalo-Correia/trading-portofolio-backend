b9283d81b09f406a8e41f4fdec1e878da66e51e6
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2022-03-09 17:11:17 -0100

    created dotnet 6 project

--- TradingPortofolioBackend/TradingPortofolioBackend/WeatherForecast.cs
+++ TradingPortofolioBackend/TradingPortofolioBackend/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace TradingPortofolioBackend
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace TradingPortofolioBackend
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
